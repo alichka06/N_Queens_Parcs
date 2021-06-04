250ef3cf098a8a4b122b051fd31c50f3b2dc8549
Alina Fedyna <45203286+alichka06@users.noreply.github.com>
2021-06-05 00:01:58 +0300

    Add files via upload

---
+++ MainIModule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using Parcs;
+
+
+namespace Queens
+{
+    class MainIModule : MainModule
+    {
+        private static int workers;
+        private static int n;
+        public static void Main(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                throw new ArgumentException("Options is not correct");
+            }
+            Int32.TryParse(args[0], out workers);
+            Int32.TryParse(args[1], out n);
+
+            var job = new Job();
+            job.AddFile(Assembly.GetExecutingAssembly().Location);
+
+            (new MainIModule()).Run(new ModuleInfo(job, null));
+            Console.WriteLine("Press ESC to exit");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+
+        public override void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            Console.WriteLine("workers: "+ workers);
+            Console.WriteLine("n: " + n);
+
+            var points = new IPoint[workers];
+            var channels = new IChannel[workers];
+
+            for (int i = 0; i < workers; ++i)
+            {
+                points[i] = info.CreatePoint();
+                channels[i] = points[i].CreateChannel();
+                points[i].ExecuteClass("Queens.QueensIModule");
+            }
+
+            int step = n / workers;
+            for (int i = 0; i < workers - 1; ++i)
+            {
+                channels[i].WriteData(i * step);
+                channels[i].WriteData(i * step + step);
+                channels[i].WriteData(n);
+                // Console.WriteLine("chanel " + i + " range: " + (i * step).ToString() + " " + (i * step + step).ToString());
+            }
+            channels[workers - 1].WriteData((workers - 1) * step);
+            channels[workers - 1].WriteData(n);
+            channels[workers - 1].WriteData(n);
+            // Console.WriteLine("chanel " + (workers - 1) + " range: " + ((workers - 1) * step).ToString() + " " + (n).ToString());
+
+            DateTime time = DateTime.Now;
+            Console.WriteLine("Waiting for result...");
+
+            int res = 0;
+            for (int i = 0; i < workers; ++i)
+            {
+                res += channels[i].ReadInt();
+            }
+
+            Console.WriteLine("Result found: res = {0}, time = {1}", res, Math.Round((DateTime.Now - time).TotalSeconds, 3));
+        }
+    }
+}
---
+++ QueensIModule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Parcs;
+
+namespace Queens
+{
+    class QueensIModule : IModule
+    {
+        private static bool Safe(int new_row, int new_col, List<int> sol)
+        {
+            for (int r = 0; r < sol.Count; r++)
+            {
+                if ((sol[r] == new_col) ||
+                    (sol[r] + r == new_col + new_row) ||
+                    (sol[r] - r == new_col - new_row))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static int CountQ(int n_row, int width, List<int> sol)
+        {
+            int all_res = 0;
+            if (sol.Count == width)
+            {
+                all_res += 1;
+            }
+            else
+            {
+                for (int n_col = 0; n_col < width; n_col++)
+                {
+                    // Console.WriteLine(n_row.ToString() + " " + n_col.ToString() + "  " + String.Join(", ", sol));
+                    if (Safe(n_row, n_col, sol))
+                    {
+                        // Console.WriteLine(n_row.ToString() + " " + n_col.ToString());
+                        List<int> new_list = new List<int>(sol);
+                        new_list.Add(n_col);
+                        all_res += CountQ(n_row + 1, width, new_list);
+                    }
+                }
+            }
+            return all_res;
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            int a = info.Parent.ReadInt();
+            int b = info.Parent.ReadInt();
+            int n = info.Parent.ReadInt();
+
+            Console.WriteLine(a.ToString() + " " + b.ToString());
+
+            int res = 0;
+            for (int i = a; i < b; i++)
+            {
+                List<int> list = new List<int>();
+                list.Add(i);
+                // Console.WriteLine(a.ToString() + " " + b.ToString() + "  " + String.Join(", ", list));
+                res += CountQ(1, n, list);
+            }
+            info.Parent.WriteData(res);
+        }
+    }
+}
